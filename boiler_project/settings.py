88f0dff7bd5380b4cf31d23901d27548dfaa7869
TMandley <51174167+TMandley@users.noreply.github.com>
2022-01-17 09:16:29 -0700

    Update settings.py

--- boiler_project/settings.py
+++ boiler_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=+oom942q!v2zm5h(z#nqfq_zv#jf(^56ubm2k3i1@y+_$y(0j'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
